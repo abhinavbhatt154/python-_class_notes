0610ff65b2f001432c9a6cf54f1c576cea510cb7
abhinavbhatt154 <abhinavbhattabhinav154@gmail.com>
2026-08-20 12:27:07 +0530

    completed first lec of 20 aug

---
+++ lab20aug.py
@@ -0,0 +1,11 @@
+firstname="Abhinav"
+lastname="Bhatt"
+full_name = firstname + " " + lastname
+print("first string:", firstname)
+print("second string:", lastname)
+print("concatinated string", full_name)
+print("first character :", firstname[0])
+print("Sub-string from index 0 to 3:", firstname[0:4])
+print("last name using slicing:", full_name[7:])
+print("reversing the string:", full_name[::-1])
+    
