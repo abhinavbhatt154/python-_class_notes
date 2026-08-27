2f28b9bf58a2a7c67896b5060013a75495f876db
abhinavbhatt154 <abhinavbhattabhinav154@gmail.com>
2026-08-27 12:41:25 +0530

    completed first lec of 27 aug

---
+++ 27aug.py
@@ -0,0 +1,27 @@
+# exception Exception - event occurring during execution of program that disrupts normal flow of program . these are the situations in which pyhton interpreter cant handle
+
+#WHY IT IS DANGEROUS- 
+#1.leads to sudden termination of programs
+#2. can block the applications
+#3. data loss can occur
+#4.corrupt data files.
+
+#EXCEPTION VS ERROR-
+#ERROR cannot be handled 
+#	can be handled using exception handling syntax
+#4 blocks provide in exception handling in python:
+#write , except, else, find block
+
+# x=int(input("enter a no 1"))
+# y= int(input("enter a no 2"))
+# div= x/y
+# print(div)
+#it will show zero division error if y=0 because we cant divide by 0. so we can handle this using exception handling syntax
+
+x=int(input("enter a no 1:"))
+y= int(input("enter a no 2:"))
+try:
+    div= x/y
+    print(div)
+except ZeroDivisionError:
+    print("Error: Division by zero is not allowed.")
---
+++ garbage.py
@@ -0,0 +1,8 @@
+arr=['a','ab','b','d','c']
+for i in range(len(arr)):
+    for j in range(i+1,len(arr)):
+           
+       print (arr[i],arr[j]) 
+    
+
+   
